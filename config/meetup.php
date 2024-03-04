67bd4df6761814ad7da39d6cad451993667de0e3
Glenn Forrest <glennjamesforrest@gmail.com>
2024-03-04 18:29:39 +0800

    wip

---
+++ config/meetup.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'url' => 'https://www.meetup.com/perth-laravel-php-meetup/',
+    'members' => 32,
+];
---
+++ resources/views/components/about.blade.php
@@ -0,0 +1,63 @@
+<div id="about" class="py-24 sm:py-32">
+    <div class="mx-auto max-w-7xl px-6 lg:px-8">
+        <div class="mx-auto max-w-2xl lg:text-center">
+            <h2 class="text-base font-semibold leading-7 text-indigo-400">
+                Connect, Learn, Grow
+            </h2>
+
+            <p class="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">
+                Connect · Learn · Grow
+            </p>
+
+            <p class="mt-6 text-lg leading-8 text-gray-300">
+                Perth Laravel Meetup is a community of Laravel enthusiasts in Perth, Western Australia. This group is all about building connections with fellow Laravel developers, sharing knowledge, and fostering collaboration in the PHP community. Whether you are a seasoned Laravel developer or just getting started, join us for regular meetups where we discuss the latest trends, tools, and techniques in Laravel development.
+            </p>
+        </div>
+
+        <div class="mx-auto mt-16 max-w-3xl sm:mt-20 lg:mt-24 lg:max-w-none">
+            <dl class="grid max-w-xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-3">
+                <div class="flex flex-col">
+                    <dt class="flex items-center gap-x-3 text-base font-semibold leading-7 text-white">
+                        <x-heroicon-s-calendar class="text-indigo-400 h-5 w-5 flex-none" />
+
+                        Meetup Schedule
+                    </dt>
+
+                    <dd class="mt-4 flex flex-auto flex-col text-base leading-7 text-gray-300">
+                        <p class="flex-auto">
+                            Join us every quarter for exciting meetups where we dive into the latest trends, best practices, and real-world applications of Laravel and PHP.
+                        </p>
+                    </dd>
+                </div>
+
+                <div class="flex flex-col">
+                    <dt class="flex items-center gap-x-3 text-base font-semibold leading-7 text-white">
+                        <x-heroicon-s-users class="text-indigo-400 h-5 w-5 flex-none" />
+
+                        Community
+                    </dt>
+
+                    <dd class="mt-4 flex flex-auto flex-col text-base leading-7 text-gray-300">
+                        <p class="flex-auto">
+                            Our community is driven by passion and fueled by collaboration. Whether you're a seasoned developer or just starting your journey, you'll find a supportive environment to grow and learn.
+                        </p>
+                    </dd>
+                </div>
+
+                <div class="flex flex-col">
+                    <dt class="flex items-center gap-x-3 text-base font-semibold leading-7 text-white">
+                        <x-heroicon-s-sparkles class="text-indigo-400 h-5 w-5 flex-none" />
+
+                        Share your Expertise
+                    </dt>
+
+                    <dd class="mt-4 flex flex-auto flex-col text-base leading-7 text-gray-300">
+                        <p class="flex-auto">
+                            Share your expertise and passion for Laravel and PHP by becoming a speaker at our meetups!
+                        </p>
+                    </dd>
+                </div>
+            </dl>
+        </div>
+    </div>
+</div>
