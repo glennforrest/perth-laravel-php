99885ad9c2f5694669352b1fa6ca6092dda70c3e
Glenn Forrest <glennjamesforrest@gmail.com>
2024-03-06 22:02:06 +0800

    wip

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -2,7 +2,7 @@
     <x-hero />
 
     <div class="xl:mx-auto xl:max-w-7xl xl:px-8 isolate">
-        <img src="/images/hero.jpg" alt="People networking" class="aspect-[9/4] w-full object-cover object-top xl:rounded-3xl">
+        <img src="../images/hero.jpg" alt="People networking" class="aspect-[9/4] w-full object-cover object-top xl:rounded-3xl">
     </div>
 
     <x-about />
