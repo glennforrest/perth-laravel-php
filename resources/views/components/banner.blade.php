f7b58fde694e2ce2d5f5c04dd643b35a572c2655
glenn <glennjamesforrest@gmail.com>
2024-04-03 10:59:07 +0800

    first event link for banner

--- resources/views/components/banner.blade.php
+++ resources/views/components/banner.blade.php
@@ -1,7 +1,7 @@
 @if (now()->isBefore('2024-04-18'))
     <div class="flex justify-center items-center gap-x-6 overflow-hidden bg-primary py-3 px-6 sm:px-3.5">
         <p class="text-base/5 font-semibold">
-            Perth Laravel PHP is having it's first meetup on the 17th of April 2024. <a href="#" class="whitespace-nowrap underline font-bold">Reserve your spot&nbsp;<span aria-hidden="true">→</span></a>
+            Perth Laravel PHP is having it's first meetup on the 17th of April 2024. <a href="https://www.meetup.com/perth-laravel-php-meetup/events/300181749/" class="whitespace-nowrap underline font-bold">Reserve your spot&nbsp;<span aria-hidden="true">→</span></a>
         </p>
     </div>
 @endif
