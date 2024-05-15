c888823fecd1bc6fea4babe9b65c9433ddb3a130
glenn <glennjamesforrest@gmail.com>
2024-05-16 07:51:54 +0800

    chore: update meetup link with public url

--- resources/views/components/banner.blade.php
+++ resources/views/components/banner.blade.php
@@ -1,7 +1,7 @@
 @if (now()->isBefore('2024-06-27'))
     <div class="flex justify-center items-center gap-x-6 overflow-hidden bg-primary py-3 px-6 sm:px-3.5">
         <p class="text-base/5 font-semibold">
-            Perth Laravel PHP is having it's next meetup on Wednesday 26 June 2024. <a href="https://www.meetup.com/perth-laravel-php-meetup/events/301031385/" class="whitespace-nowrap underline font-bold">Reserve your spot&nbsp;<span aria-hidden="true">→</span></a>
+            Perth Laravel PHP is having it's next meetup on Wednesday 26 June 2024. <a href="https://www.meetup.com/perth-laravel-php-meetup/events/301031385/?utm_medium=referral&utm_campaign=share-btn_savedevents_share_modal&utm_source=link" class="whitespace-nowrap underline font-bold">Reserve your spot&nbsp;<span aria-hidden="true">→</span></a>
         </p>
     </div>
 @endif
