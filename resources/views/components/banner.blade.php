24fe15a9b066c8f983622a4b2424c5e245babc86
Glenn Forrest <glennjamesforrest@gmail.com>
2025-02-11 12:09:22 +0800

    feat: update banner for Feb meetup.

--- resources/views/components/banner.blade.php
+++ resources/views/components/banner.blade.php
@@ -1,7 +1,7 @@
-@if (now()->isBefore('2024-06-27'))
+@if (now()->isBefore('2025-02-24'))
     <div class="flex justify-center items-center gap-x-6 overflow-hidden bg-primary py-3 px-6 sm:px-3.5">
         <p class="text-base/5 font-semibold">
-            Perth Laravel PHP is having it's next meetup on Wednesday 26 June 2024. <a href="https://www.meetup.com/perth-laravel-php-meetup/events/301031385/?utm_medium=referral&utm_campaign=share-btn_savedevents_share_modal&utm_source=link" class="whitespace-nowrap underline font-bold">Reserve your spot&nbsp;<span aria-hidden="true">→</span></a>
+            Perth Laravel PHP is having it's next meetup on Sunday 23 February 2025. <a href="https://www.meetup.com/perth-laravel-php-meetup/events/306008137/" class="whitespace-nowrap underline font-bold">Reserve your spot&nbsp;<span aria-hidden="true">→</span></a>
         </p>
     </div>
 @endif
